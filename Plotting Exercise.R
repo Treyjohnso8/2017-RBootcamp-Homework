7846c325a880838f8fd1e5dc7a8b64bfa8c5f53c
Trey Johnson III <32042537+Treyjohnso8@users.noreply.github.com>
2017-09-24 15:27:23 -0700

    Add files via upload

--- Plotting Exercise.R
+++ Plotting Exercise.R
@@ -20,7 +20,7 @@ get_heights<-function(heights){
   return(heights)
 }
 get_heights(heights)
-
+heights
 #2. Within your function, compute the average height from your “heights” vector.
 get_heights<-function(heights){   
   heights<-mean(rnorm(1000,mean=69,sd=10))
@@ -37,20 +37,33 @@ get_heights(heights)
 #4. Use a “for” loop to call your “get_heights” function 1000 times, with taking a sample of size
 #100 from the population. Save the mean height from each replicate in a vector called
 #“mean_heights_100”.
+get_heights<-function(sample_heights=1000){   
+  heights<-rnorm(sample_heights,mean=69,sd=10)
+  return(heights)
+}
+get_heights(heights) 
+
 for (i in 1:1000){
-  get_heights(heights)
+  mean_heights_100<-get_heights(sample_heights = 100)
 } 
-get_heights(sample_heights=100)
+mean_heights_100
 
 #5. Use a “for” loop to call your “get_heights” function 1000 times, with taking a sample of size
 #1000 from the population. Save the mean height from each replicate in a vector called
 #“mean_heights_1000”.
-get_heights() #Based at 1000                                                                                  
+for (i in 1:1000){
+  mean_heights_1000<-data.frame(sample_heights = 1000)
+} 
+mean_heights_1000                                                                                
                                                                                    
 #6. Plot a histogram of the distribution of the average heights for your sample size of 
 #100 and 1000 individuals. The two sets of data should be plotted on the same axes. 
 #Add a legend. Label the axes. Plot the data from the 100 samples in red and the data
 #from the 1000 samples in blue. Your plot should look something like the one shown on
 #the next page.
-hist(c(get_heights=1000,get_heights=100))
-     
+bins<-seq(0,50,by=1)
+hist1000<-hist(mean_heights_1000)
+hist100<-hist(mean_heights_100)
+par(mfrow=c(1,1), mar=c(4, 4, 3, 2))
+barplot(c(hist1000,mean_heights_100),col=c(2,4),beside=T,
+      xlab="Value",ylab="Count")
